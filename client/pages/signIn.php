f6750d0d8e6ba6fe3e0f0e4e755c4de8477ee8d7
Lucas-ylm <120712755+Lucas-ylm@users.noreply.github.com>
2023-04-27 11:20:51 +0200

    beaucoup de modifs

--- client/pages/signIn.php
+++ client/pages/signIn.php
@@ -8,19 +8,21 @@ require_once '../TPL/header.php';
             <p class="descForm">
                 Veuillez remplir tous les champs :
             </p>
-            <label><input type="radio" name="bouton" value="option1">Participant</label>
-            <label><input type="radio" name="bouton" value="option2">Organisateur</label>
-            <input type="nom" class="iNOM1" placeholder="Username">
-            <input type="email" class="iEMAIL1" placeholder="Adresse e-mail">
-            <input type="Mot de passe" class="iMDP1" placeholder="Mot de passe">
+        <form action="/server/Logger-PHP/register" method="POST">
+            <label><input name="role" type="radio" name="bouton" value="option1" checked>Participant</label>
+            <label><input name="role" type="radio" name="bouton" value="option2">Organisateur</label>
+            <input name="username" type="nom" class="iNOM1" placeholder="Username">
+            <input name="mail" type="email" class="iMDP" placeholder="Adresse e-mail">
+            <input name="password" type="Mot de passe" class="iMDP" placeholder="Mot de passe">
             <p class="mdpDesc">Votre mot de passe comprend 8 caractères minimum, avec au moins une lettre majuscule, une lettre miniscule et un chiffre (0-9)</p>
-            <input type="pays" class="iCountry" placeholder="Pays">
-            <input type="pays" class="iCodePostal" placeholder="Code Postal">
-            <input type="pays" class="iVille" placeholder="Ville">
-            <input type="pays" class="iTel" placeholder="Numéro de téléphone">
-            <button>
-                Créer mon compte 
-            </button>
+            <input class="iCountry" placeholder="Pays">
+            <input class="iCodePostal" placeholder="Code Postal">
+            <input class="iVille" placeholder="Ville">
+            <input class="iTel" placeholder="Numéro de téléphone">
+            <input type="submit" class="button" value="Créer mon compte">
+                 
+            
+        </form>
         </main>
         <script src="../JS/index.js"></script>
     </body>
